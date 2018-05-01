b6d9203ac6414dfe5e9acef33271f5e041b1eb5c
jacques@decodemtl.com <jacques@decodemtl.com>
2018-05-01 13:45:05 -0400

    first commit

---
+++ public/app.js
@@ -0,0 +1,102 @@
+// Remember: no copy pasting!
+
+// Controlled input. This is similar to what you did in react.
+function addItemInputChanged() {
+    setState({ addItemInput: event.target.value });
+}
+
+// Controlled input. This is similar to what you did in react.
+function nameInputChanged() {
+    setState({ listNameInput: event.target.value });
+}
+
+// Don't try to understand the body of this function. You just 
+// need to understand what each parameter represents
+function makeHTTPRequest(meth, url, body, cb) {
+    fetch(url, {
+        body: body,
+        method: meth
+    })
+        .then(response => response.text())
+        .then(responseBody => cb ? cb(responseBody) : undefined)
+}
+
+// We're going to try and stick with React's way of doing things
+let state = {
+    items: [],
+    addItemInput: "", // The contents of the add item input box
+    listNameInput: "", // The contents of the input box related to changing the list
+    listName: "grocery list"
+}
+
+// Calling rerender changes the UI to reflect what's in the state
+
+function rerender() {
+    let inputElement = document.getElementById('itemInput');
+    inputElement.value = state.addItemInput; // you can ignore this line
+
+    let lne = document.getElementById('listNameInputChanged');
+    lne.value = state.listNameInput; // you can ignore this line
+
+
+    let listNameElement = document.getElementById('listName')
+    listNameElement.innerText = state.listName;
+
+    let d = document.getElementById("items");
+    d.innerHTML = '';
+    if (state.items[state.listName]) {
+        state.items[state.listName].forEach(item => {
+            let li = document.createElement("li");
+            li.innerText = item;
+            d.appendChild(li)
+        })
+    }
+}
+
+// Our good friend setState paying us a visit from ReactVille
+function setState(newState) {
+    if (newState.items !== undefined) state.items = newState.items;
+    if (newState.addItemInput !== undefined) state.addItemInput = newState.addItemInput;
+    if (newState.listNameInput !== undefined) state.listNameInput = newState.listNameInput;
+    if (newState.listName !== undefined) state.listName = newState.listName;
+    rerender();
+}
+
+function sendItemToServer(it, ln) {
+    // This function is so short it could be inlined
+    let cb = (itemsFromServer) => {
+        let parsedItems = JSON.parse(itemsFromServer)
+        setState({ items: parsedItems })
+    }
+    setState({ addItemInput: '' });
+    makeHTTPRequest('POST',
+        '/addItem',
+        JSON.stringify({ item: it, listName: ln }),
+        cb)
+}
+
+// When you submit the form, it sends the item to the server
+function addItemSubmit() {
+    event.preventDefault();
+
+    sendItemToServer(state.addItemInput, state.listName)
+}
+
+function listNameSubmit() {
+    event.preventDefault();
+    setState({listName: state.listNameInput, listNameInput: '' });
+}
+
+// When the client starts he needs to populate the list of items
+function populateItems() {
+    let cb = itemsString => {
+        let itemsParsed = JSON.parse(itemsString)
+        setState({ items: itemsParsed })
+    }
+
+    makeHTTPRequest('POST', '/items', undefined, cb)
+}
+
+// We define a function and then call it right away. I did this to give the file a nice structure.
+populateItems();
+rerender();
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const fs = require('fs')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.raw({ type: '*/*' }))
+
+
+// The following two endpoints are so that the browser can load the HTML and Javascript
+app.get('/', (req, res) => res.send(fs.readFileSync('./public/index.html').toString()))
+app.get('/app.js', (req, res) => res.send(fs.readFileSync('./public/app.js').toString()))
+
+// 
+let serverState = {
+    items: {}
+}
+
+app.post('/items', (req, res) => {
+    res.send(JSON.stringify(serverState.items));
+})
+
+app.post('/addItem', (req, res) => {
+    // Remember: the body of an HTTP response is just a string.
+    // You need to convert it to a javascript object
+    let parsedBody = JSON.parse(req.body.toString())
+    // This is just a convenience to save some typing later on
+    let listName = parsedBody.listName;
+    // If the list doesn't exist, create it
+    if (!serverState.items[listName]) { serverState.items[listName] = [] }
+    // The following could be rewritten in a shorter way using push.
+    // Try rewriting it. It will help you understand it better.
+    serverState.items[listName] = serverState.items[listName].concat(parsedBody.item)
+    res.send(JSON.stringify(serverState.items));
+})
+
+app.listen(4000, () => console.log('Example app listening on port 4000!'))
